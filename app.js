b77b85e2a987ef8783fe4bf427fd64dd894d023e
ekekek201-dev <ekekek201@gmail.com>
2026-06-02 18:09:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -233,8 +233,8 @@ function saveFishingPoint() {
             if (depthResponse.success && depthResponse.rawItems && depthResponse.rawItems.length > 0) {
                 
                 newPoint.depth = depthResponse.depth + " m";
-                console.log("API 응답 데이터:", depthResponse.depth);
-                console.log("newpoint 데이터:", newPoint.depth);
+                console.log("API 응답 데이터:", depthResponse.distMeter);
+                console.log("API 응답 데이터:", newPoint.depth);
                 let addedCount = 0;               
                 
                 depthResponse.rawItems.forEach(function(pt) { //foreach는 반복문
