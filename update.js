0b21447baaf9937954c04039b77a27fa5ac499f2
ekekek201-dev <ekekek201@gmail.com>
2026-06-11 12:43:18 +0900

    Update update.js

--- update.js
+++ update.js
@@ -11,6 +11,10 @@ const now = new Date(
     })
 );
 
+if (!fs.existsSync('./data/temp')) {
+    fs.mkdirSync('./data/temp', { recursive: true });
+}
+
 const resultFile =
     `data/temp/${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}.json`;
 
