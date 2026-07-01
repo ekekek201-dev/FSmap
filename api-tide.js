141fe96d288542a64217f52e3939fe40e3114f93
ekekek201-dev <ekekek201@gmail.com>
2026-07-01 23:25:40 +0900

    Update api-tide.js

--- api-tide.js
+++ api-tide.js
@@ -2,79 +2,7 @@
 const tideCache = {};
 const tideCache_temp = {};
 
-async function getTideData_bak(obsCode, reqDate, time) {
-    const req = reqDate.replaceAll('-', '');
-    const cacheKey = `${obsCode}_${reqDate}`;
-    const API_KEY = '27441bf5450704615e12175286ff5b62c526acc1bed6e9690d3fef6fc7e9102f';
-    const url = `https://apis.data.go.kr/1192136/tideFcstHghLw/GetTideFcstHghLwApiService?serviceKey=${API_KEY}&obsCode=${obsCode}&type=json&reqDate=${req}`;
 
-    for (let attempt = 1; attempt <= 3; attempt++) {
-    try {
-
-        let items;
-
-        // 첫 시도만 캐시 사용
-        if (attempt === 1 && tideCache[cacheKey]) {
-            console.log('조석 캐시 사용');
-            items = tideCache[cacheKey];
-        } else {
-            console.log(`조석 API 호출 (${attempt}/3)`);
-
-            const response = await fetch(url);
-            const data = await response.json();
-           
-    
-            if (!data?.body?.items?.item) {
-                throw new Error('API 응답에 item이 없음');
-            }
-
-            items = data.body.items.item;
-            console.log(items);
-
-            // 정상 데이터면 캐시 갱신
-            tideCache[cacheKey] = items;
-        }
-
-        items.sort((a, b) =>
-            a.predcDt.localeCompare(b.predcDt)
-        );
-
-        const targetDate = new Date(`${reqDate}T${time}:00`);
-
-        const nextTide = items.find(item =>
-            new Date(item.predcDt.replace(' ', 'T')) > targetDate
-        );
-
-        const prevTide = [...items]
-            .reverse()
-            .find(item =>
-                new Date(item.predcDt.replace(' ', 'T')) < targetDate
-            );
-
-        if (!prevTide || !nextTide) {
-            throw new Error('만조/간조 데이터 찾기 실패');
-        }
-
-        return {
-            prevTide: `${prevTide.predcDt.split(' ')[1]} (${prevTide.predcTdlvVl})`,
-            nextTide: `${nextTide.predcDt.split(' ')[1]} (${nextTide.predcTdlvVl})`
-        };
-
-    } catch (e) {
-
-        // 캐시가 문제일 가능성 있으니 제거
-        delete tideCache[cacheKey];
-
-        console.error(`조석정보 조회 실패 (${attempt}/3):`, e);
-
-        if (attempt === 3) {
-            return null;
-        }
-
-        await new Promise(resolve => setTimeout(resolve, 500));
-    }
-}
-}
 
 
 
