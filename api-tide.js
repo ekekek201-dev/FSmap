6b7376fa61a8f72faa316a2e4d61a636795035c2
ekekek201-dev <ekekek201@gmail.com>
2026-07-15 20:20:58 +0900

    Update api-tide.js

--- api-tide.js
+++ api-tide.js
@@ -341,7 +341,7 @@ async function getWaterTemp_c(obsCode, time) {
 }
 
 async function get_weather(lat,lot, reqDate, targetTime) {    
-    const cacheKey = `${reqDate}_${time}`;
+    const cacheKey = `${reqDate}_${targetTime}`;
     const url =
         `https://archive-api.open-meteo.com/v1/archive?` +
         `latitude=${lat}&longitude=${lot}&start_date=${reqDate}&end_date=${reqDate}` +
